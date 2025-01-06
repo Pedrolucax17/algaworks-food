d6d4cbce5be82afd014d0831c183c12469305b9e
Pedro Lucas <pl.lucassouza@gmail.com>
2025-01-06 08:54:27 -0300

    Adicionando as dependências iniciais

---
+++ src/main/java/com/algafoods/AlgawfoodApplication.java
@@ -0,0 +1,13 @@
+package com.algafoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgawfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgawfoodApplication.class, args);
+	}
+
+}
