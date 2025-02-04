847121f7dc6d50fb8ca3adbcb2f73d482de76f57
Pedro Lucas <pl.lucassouza@gmail.com>
2025-02-04 17:54:53 -0300

    Adicionando JPA e MySQL Driver e configurando o datasource

--- src/main/java/com/algafoods/notificacao/NivelUrgencia.java
+++
@@ -1,8 +0,0 @@
-package com.algafoods.notificacao;
-
-public enum NivelUrgencia {
-	
-	URGENTE,
-	NORMAL
-
-}
--- src/main/java/com/algafoods/notificacao/Notificador.java
+++
@@ -1,8 +0,0 @@
-package com.algafoods.notificacao;
-
-import com.algafoods.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-}
