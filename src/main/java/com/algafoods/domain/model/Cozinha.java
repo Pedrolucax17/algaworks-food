58a461d786f51d5151095e771faf0766879319be
Pedro Lucas <pl.lucassouza@gmail.com>
2025-03-10 09:59:53 -0300

    Mapeando Relacionamentos

--- src/main/java/com/algafoods/domain/model/Cozinha.java
+++ src/main/java/com/algafoods/domain/model/Cozinha.java
@@ -1,9 +1,15 @@
 package com.algafoods.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,6 +22,10 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String nome;		
+	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
--- src/main/java/com/algafoods/domain/model/Restaurante.java
+++ src/main/java/com/algafoods/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.algafoods.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +10,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -29,5 +33,13 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id")
+	)
+	private List<FormaPagamento> formasPagamentos = new ArrayList<>();
 	
 }
