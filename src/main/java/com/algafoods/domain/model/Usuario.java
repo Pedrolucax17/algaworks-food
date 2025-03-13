636cd7b5c4e26a4f4d55d374dfac70f4d446b8ea
Pedro Lucas <pl.lucassouza@gmail.com>
2025-03-13 08:40:17 -0300

    Modelando a classe usuario e implementando relacionamento manyToMany na classe Grupo

--- src/main/java/com/algafoods/domain/model/Usuario.java
+++ src/main/java/com/algafoods/domain/model/Usuario.java
@@ -1,37 +1,38 @@
 package com.algafoods.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
 @Entity
+@Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Grupo {
+public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
-
+	
 	@Column(nullable = false)
 	private String nome;
-
-	@ManyToMany
-	@JoinTable(
-			name = "grupo_permissao",
-			joinColumns = @JoinColumn(name = "grupo_id"),
-			inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-	private List<Permissao> permissoes = new ArrayList<>();
-
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	@CreationTimestamp
+	@Column(nullable = false)
+	private LocalDateTime dataCadastro;
+	
 }
