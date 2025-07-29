adc8f9b9de8090358b0b1ac3e1b657dd48e3f91e
Pedro Lucas <pl.lucassouza@gmail.com>
2025-07-29 11:01:30 -0300

    Criando constraints de validacao customizada com implementacao de ConstraintValidator

--- src/main/java/com/algafoods/domain/model/Restaurante.java
+++ src/main/java/com/algafoods/domain/model/Restaurante.java
@@ -27,7 +27,6 @@ import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.groups.ConvertGroup;
 import jakarta.validation.groups.Default;
 import lombok.Data;
